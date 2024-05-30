bfb9abfbb9115ce2d1acf41d8a79166edd110fa5
Isabelle <116334027+isabellelbgn@users.noreply.github.com>
2024-05-30 19:33:56 +0800

    Add e-commerce server
    Build backend features for handling product catalog, orders, payments, suppliers, customers, and user authentication

---
+++ server/README.md
@@ -0,0 +1,1 @@
+# sad2-ecommerce
---
+++ server/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 60000,
+});
+
+dbConnection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+module.exports = { dbConnection };
