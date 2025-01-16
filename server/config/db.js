1bba7833644415b133fbdd8e0e1e7f908b44a991
isabellelbgn <116334027+isabellelbgn@users.noreply.github.com>
2025-01-16 20:23:38 +0800

    refactor(config): move MySQL database connection to config folder

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const dbConnection = mysql.createConnection({
+const dbConnection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,12 +12,12 @@ const dbConnection = mysql.createConnection({
   connectTimeout: 60000,
 });
 
-dbConnection.connect((err) => {
+dbConnection.getConnection((err) => {
   if (err) {
     console.error("Error connecting to MySQL:", err);
-    return;
+    process.exit(1);
   }
   console.log("Connected to MySQL database");
 });
 
-module.exports = { dbConnection };
+module.exports = dbConnection.promise();
